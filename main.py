4fad86c1ee6fe349d5f5a693a5738c0f23936b17
Renantyk <145122226+Renantyk@users.noreply.github.com>
2025-06-21 19:30:18 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 lCam = 640
 aCam = 480
 
-
 cam = cv2.VideoCapture(0)
 cam.set(3, lCam)
 cam.set(4, aCam)
@@ -16,14 +15,19 @@ detector = MaosDetector()
 keras = Keras()
 palavra = ""
 
+tempo = 0
+intervalo = 1
+
 while True:
     success, img = cam.read()
     img = detector.encontrarMaos(img)
     lmList = detector.encontrarPosicao(img, draw=False)
     
+    letra = ""
     if len(lmList) > 0:
-        palavra = palavra + keras.instanciar(lmList)
+        letra = keras.instanciar(lmList)
     
+    cv2.putText(img, letra, (40, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
     cv2.putText(img, palavra, (40, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
     print(palavra)
     
@@ -32,7 +36,13 @@ while True:
     pTempo = cTempo
     
     cv2.imshow("Img", img)
-    cv2.waitKey(100)
+    key = cv2.waitKey(100)
+
+    tAtual = time.time()
+
+    if key == ord("c") and (tAtual - tempo > intervalo):
+        palavra += letra
+        tempo = tAtual
     
     if len(palavra) > 25:
         palavra = ""
