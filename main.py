2fc27405ebb5b49224f86e5789f97a680cb64539
andreckw <114355564+andreckw@users.noreply.github.com>
2025-06-21 18:37:56 -0300

    agora tem palavra

--- main.py
+++ main.py
@@ -14,20 +14,25 @@ pTempo = 0
 
 detector = MaosDetector()
 keras = Keras()
+palavra = ""
 
 while True:
     success, img = cam.read()
-    img = detector.encontrarMaos(img, draw=False)
+    img = detector.encontrarMaos(img)
     lmList = detector.encontrarPosicao(img, draw=False)
     
     if len(lmList) > 0:
-        letra = keras.instanciar(lmList)
-        cv2.putText(img, f"Letra: {letra}", (40, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+        palavra = palavra + keras.instanciar(lmList)
     
+    cv2.putText(img, palavra, (40, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+    print(palavra)
     
     cTempo = time.time()
     fps = 1/(cTempo-pTempo)
     pTempo = cTempo
     
     cv2.imshow("Img", img)
-    cv2.waitKey(10)
+    cv2.waitKey(100)
+    
+    if len(palavra) > 25:
+        palavra = ""
